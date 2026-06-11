f603b6d7aeb7c510fbc32839768398fc15158381
Carlos Eduardo Bueno de Souza <carlosbueno_53@hotmail.com>
2026-06-10 21:32:01 -0300

    Aprendendo a conectar banco de dados com Java

---
+++ Agenda_ETECIA/src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import Controller.*;
+import View.*;
+
+
+public class Main {
+
+   
+    public static void main(String[] args) {
+       new Inicio_GUI().setVisible(true);
+    }
+    
+}
